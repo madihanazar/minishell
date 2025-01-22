128774fea4ff48d4ab4992d971249d6ce47a4caa
Nikhil Kunnathu <nkunnath@lab3r7s14.42abudhabi.ae>
2025-01-22 15:24:07 +0400

    Created a folder containing utility files

---
+++ Utilities/string_utils.c
@@ -0,0 +1,293 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   string_utils.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/23 20:37:01 by mnazar            #+#    #+#             */
+/*   Updated: 2025/01/11 20:55:06 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int ft_strlen(const char *str)
+{
+	int i;
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+int ft_strcmp(const char *s1, const char *s2)
+{
+    while (*s1 && (*s1 == *s2))
+    {
+        s1++;
+        s2++;
+    }
+    return ((unsigned char)*s1 - (unsigned char)*s2);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*sub;
+	size_t	i;
+
+	i = 0;
+	if (!s)
+		return (NULL);
+	if (start >= (unsigned int) ft_strlen(s))
+		return (ft_strdup(""));
+	if (len > (size_t)ft_strlen(s + start))
+		len = ft_strlen(s + start);
+	sub = (char *)malloc(len + 1 * sizeof(char));
+	if (sub == NULL)
+		return (NULL);
+	while (i < len)
+	{
+		sub[i] = s[start + i];
+		i++;
+	}
+	sub[i] = '\0';
+	return (sub);
+}
+
+char *ft_strtrim(char const *s1, char const *set)
+{
+   
+	size_t	i;
+
+	if (!s1 || !set)
+		return (NULL);
+	while (*s1 && ft_strchr(set, *s1))
+		s1++;
+	i = ft_strlen(s1);
+	while (i && ft_strchr(set, s1[i]))
+		i--;
+	return (ft_substr(s1, 0, i + 1));
+}
+char *ft_strchr(const char *str, int c) 
+{
+    size_t i;
+    i = 0;
+
+    while (str[i] != '\0')
+    {
+        if (str[i] == (char)c)
+         {
+            return ((char *)(str + i));
+         }   
+        i++;
+    }
+    if ((char)c == '\0')
+        return ((char *)(str + i));
+    return (NULL);
+}
+
+char *ft_strdup(char *s)
+{
+    char* ans;
+    size_t i;
+    i = 0;
+
+    while (s[i] != '\0')
+    {
+        i++;
+    }
+    ans = (char *)malloc(i + 1 * sizeof(char));
+    if (ans == NULL)
+        return (NULL);
+    i = 0;
+    while (s[i] != '\0')
+    {
+        ans[i] = s[i];
+        i++;
+    }
+    ans[i] = '\0';
+    return (ans);
+}
+
+int	ft_isalnum(int c)
+{
+	if ((c >= 48 && c <= 57) || (c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+		return (1);
+	return (0);
+}
+
+int	ft_isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+	return (0);
+}
+
+int	ft_strncmp(const char *str1, const char *str2, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	if (n <= 0)
+		return (0);
+	while (str1[i] != '\0' && str2[i] != '\0' && str1[i] == str2[i])
+	{
+		if (i < n - 1)
+			i++;
+		else
+			break ;
+	}
+	return ((unsigned char)str1[i] - (unsigned char)str2[i]);
+}
+
+char *ft_strcpy(char *dest, const char *src)
+{
+    size_t i;
+
+    if (!dest || !src)
+        return (NULL);
+
+    i = 0;
+    while (src[i])
+    {
+        dest[i] = src[i];
+        i++;
+    }
+    dest[i] = '\0'; // Null-terminate the destination string
+	
+    return dest;
+}
+
+void	ft_bzero(void *ptr, size_t n)
+{
+	char	*str;
+	size_t	i;
+
+	str = ptr;
+	i = 0;
+	while (i < n)
+	{
+		str[i] = '\0';
+		i++;
+	}
+}
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	i;
+	size_t	j;
+	char	*res;
+
+	i = 0;
+	j = 0;
+	if (!s1 || !s2)
+		return (NULL);
+	res = (char *)malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	if (res == NULL)
+		return (NULL);
+	while (s1[i] != '\0')
+	{
+		res[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		res[i + j] = s2[j];
+		j++;
+	}
+	res[i + j] = '\0';
+	return (res);
+}
+
+static char	**malloc_free(char **array, int position)
+{
+	int	i;
+
+	i = 0;
+	while (i < position)
+	{
+		free(array[i]);
+		i++;
+	}
+	free(array);
+	return (NULL);
+}
+
+static int	ft_word_count(const char *s, char sep)
+{
+	int	i;
+	int	word;
+
+	i = 0;
+	word = 0;
+	while (s[i])
+	{
+		if (s[i] != sep && (s[i + 1] == sep || s[i + 1] == '\0'))
+			word++;
+		i++;
+	}
+	return (word);
+}
+
+static char	*ft_get_word(const char *s, int start, int end)
+{
+	char	*word;
+	int		i;
+
+	i = 0;
+	word = (char *)malloc(sizeof(char) * ((end - start + 1) + 1));
+	if (!word)
+	{
+		return (NULL);
+	}
+	while (start <= end)
+		word[i++] = s[start++];
+	word[i] = '\0';
+	return (word);
+}
+
+char	**get_array(const char *s, char c, size_t i, int j)
+{
+	char	**ptr;
+	int		k;
+
+	ptr = (char **)malloc(sizeof(char *) * (ft_word_count(s, c) + 1));
+	if (!ptr)
+		return (NULL);
+	k = 0;
+	while (i <= (size_t) ft_strlen(s))
+	{
+		j = i;
+		while (s[i] && s[i] != c)
+		{
+			if (s[++i] == c || s[i] == '\0')
+			{
+				ptr[k++] = ft_get_word(s, j, i - 1);
+				if (!(ptr[k - 1]))
+					return (malloc_free(ptr, k - 1));
+			}
+		}
+		if (s[i++] == '\0')
+			break ;
+	}
+	ptr[k] = NULL;
+	return (ptr);
+}
+
+char	**ft_split(const char *s, char c)
+{
+	char	**ptr;
+
+	if (!s)
+		return (NULL);
+	ptr = get_array(s, c, 0, 0);
+	return (ptr);
+}
+
+void	ft_putstr_fd(char *s, int fd)
+{
+	if (!s || !fd)
+		return ;
+	write(fd, s, ft_strlen(s));
+}
